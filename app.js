57737084aa9a3429620722f3ad780418ec9f4ec0
marcbryan <marcboakyeflores98@gmail.com>
2019-04-08 19:30:28 +0200

    Prueba db

--- app.js
+++ app.js
@@ -16,6 +16,26 @@ var users = {
 
 var c = 0;
 
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+app.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM usuarios');
+      const results = { 'results': (result) ? result.rows : null};
+      console.log(results);
+      //res.render('pages/db', results );
+      client.release();//*/
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+
 app.set('view engine', 'ejs');
 
 // POST /login gets urlencoded bodies
