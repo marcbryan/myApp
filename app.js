c19bf8e7f4255453e52f8074e08782de96cd363c
marcbryan <marcboakyeflores98@gmail.com>
2019-04-30 11:30:28 +0200

    Fix 5 lista usuarios

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ const pool = new Pool({
 // Ruta /db -> Muestra una lista con los usuarios que hay en la base de datos de PostgreSQL
 app.get('/db', async (req, res) => {
   res.render("db");
-  var document = new JSDOM("https://secret-chamber-67247.herokuapp.com/db").window.document;
+  var document = new JSDOM("https://secret-chamber-67247.herokuapp.com/db").window;
     try {
       const client = await pool.connect();
       const result = await client.query('SELECT * FROM usuarios');
